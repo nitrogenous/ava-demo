11f1175d073bbc2aff735ff071fdf5ffdb07d065
Toprak Koç <hello@toprak.dev>
2020-06-21 16:06:03 +0300

    added mouse event functions

--- poly-script.js
+++ poly-script.js
@@ -15,6 +15,7 @@ window.onload = function () {
 
 			self.clearPoly();
 			self.createPoly(points);
+			self.setMouseEvents();
 		};
 
 		self.clearPoly = function () 
@@ -24,6 +25,7 @@ window.onload = function () {
 		};
 
 		self.createPoly = function (points, color = 'black') {
+			console.log(points);
 			if (points.length < 2) {
 				console.log('Not enought points');
 				return;
@@ -49,6 +51,39 @@ window.onload = function () {
 
 			elementOfSvg.appendChild(pathOfSvg);
 			elementOfContent.appendChild(elementOfSvg);
+		};
+
+		self.setMouseEvents = function () {
+			document.addEventListener('mousemove', self.onMouseMove);
+			document.addEventListener('mousedown', self.onMouseDown);
+			document.addEventListener('mouseup', self.onMouseUp);
+		};
+
+		self.onMouseMove = function () {
+
+		};
+
+		self.onMouseDown = function () {
+
+		};
+
+
+		self.onMouseUp = function () {
+			const poly1 = [ { x : 100, y: 100 },
+			{ x : 200, y: 50 },
+			{ x : 300, y: 50 },
+			{ x : 400, y: 200 },
+			{x: 150,y: 150}];
+			const poly2 = [ {x: 150,y: 150}, { x : 400, y: 200 },   { x : 350, y: 250 },
+			{ x : 200, y: 300 },
+			{ x : 150, y: 300 },];
+
+			//Generate the two sets of points for the split polygons
+			//An algorithm for finding interceptions of two lines can be found in https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection
+
+			self.clearPoly();
+			self.createPoly(poly1, 'blue');
+			self.createPoly(poly2, 'green');
 		};
 
 		self.init();
