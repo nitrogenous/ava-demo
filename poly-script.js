14fc775924c268ae7191913ccbf68ba6f5af717c
Toprak Koç <hello@toprak.dev>
2020-06-21 15:41:27 +0300

    Created new self-invoke script

---
+++ poly-script.js
@@ -0,0 +1,47 @@
+(function (self) {
+	var elementOfContent = document.getElementById('poly');
+
+	self.init = function () {
+		self.clearPoly();
+		self.createPoly(points);
+	};
+
+	self.clearPoly = function () {
+		console.log(document.getElementById('poly'));
+		while (elementOfContent.firstChild) {
+			elementOfContent.removeChild(elementOfContent.firstChild);
+		}
+	};
+
+	self.createPoly = function (points, color = black) {
+		if (points.length < 2) {
+			console.log('Not enought points');
+			return;
+		}
+
+		var elementOfSvg = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
+		var pathOfSvg = document.createElementNS("http://www.w3.org/2000/svg", 'path');
+		var path = 'M' + points[0].x + ' ' + points[0].y;
+
+		for(const point of points) {
+			path += ' L' + point.x + ' ' + point.y;
+		}
+	
+		path += " Z";
+	
+		svgPath.setAttribute('d', path);
+		svgPath.setAttribute('stroke', color);
+
+		svgElement.setAttribute('height', "500"); 
+		svgElement.setAttribute('width', "500");
+		svgElement.setAttribute('style', 'position: absolute;');
+		svgElement.setAttribute('fill', 'transparent');
+
+		svgElement.appendChild(svgPath);
+		content.appendChild(svgElement);
+	};
+
+	window.onload = function () {
+		self.init();
+	};
+}({}))
